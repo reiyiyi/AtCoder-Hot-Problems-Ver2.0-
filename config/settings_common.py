6f4af632c4c1687d1b7b528f9de648787bfa3930
regi <63766962+reiyiyi@users.noreply.github.com>
2021-12-01 01:05:57 +0900

    settings.py変更

--- config/settings_common.py
+++ config/settings_common.py
@@ -110,4 +110,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
